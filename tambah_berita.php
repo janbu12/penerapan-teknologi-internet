989985b76fb8f1edc25124d6a7986c62276464be
Mizan <111039118+janbu12@users.noreply.github.com>
2024-10-17 13:35:31 +0700

    Update tambah_berita.php

--- tambah_berita.php
+++ tambah_berita.php
@@ -17,21 +17,47 @@ echo "Hello, $nama!";
 
 ?>
 
-<html>
-    <center>
-        <h1>Tambah Berita</h1>
-        <form action="proses_tambah_berita.php" method="POST">
-            <table>
-                <tr>
-                    <td>Judul Berita</td>
-                    <td><input type="text" name="judul" required></td>
-                </tr>
-                <tr>
-                    <td>Isi Berita</td>
-                    <td><textarea name="isi" required></textarea></td>
-                </tr>
-            </table>
-            <input type="submit" value="Tambah Berita">
-        </form>
-    </center>
-</html>
+<html>   
+    
+ <head>   
+     <title>Buku Tamu</title>   
+ </head>   
+ <center>   
+    
+     <body>   
+     <form action=simpan_berita.php method=post enctype="multipart/form-data">   
+         <h1>FORM TAMBAH BERITA</h1>   
+         <h2>UNIKOM NEWS</h2>   
+         <hr>   
+         <table>   
+             <tr>   
+                 <td> Judul Berita Bahasa Indonesia</td>   
+                 <td> <input type=text name=judul_berita_indonesia size=50></td>   
+             </tr>   
+           <tr>   
+                 <td> Judul Berita Bahasa Inggris</td>   
+                 <td> <input type=text name=judul_berita_inggris size=50></td>   
+             </tr>   
+             <tr>   
+                 <td> Isi Berita Bahasa Indonesia  </td>   
+                 <td> <textarea rows="10" cols="50" name=isi_berita_indonesia 
+maxlength=1000 ></textarea></td>   
+             </tr>   
+             <tr>   
+                 <td> Isi Berita Bahasa Inggris    </td>   
+                 <td> <textarea rows="10" cols="50" name=isi_berita_inggris 
+maxlength=1000></textarea> </td>   
+             </tr>   
+             <tr>   
+                 <td> File Pendukung           </td>   
+                 <td> <input type="file" name="file"></td>   
+             </tr>   
+    
+         </table>   
+        <br><br>   
+         <input type=submit value=simpan name=simpan> <input type=reset value=Reset>
+   
+     </form>   
+    </body>   
+</center>   
+</html>  
